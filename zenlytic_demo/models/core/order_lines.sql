31f897e0cad0ea00c42a1d2a71e6eb4c35a08a0a
Paul Blankley <paul@zenlytic.com>
2022-10-11 17:53:28 -0600

    add restriction for duplicate order line ids

--- zenlytic_demo/models/core/order_lines.sql
+++ zenlytic_demo/models/core/order_lines.sql
@@ -2,12 +2,20 @@ with source as (
     select * from {{ source('demo_raw', 'order_lines') }}
 ),
 
+base as (
+  select 
+    row_number() over (partition by order_line_id order by order_at) as order_line_number,
+    *
+  from source
+),
+
 result as (
     select 
         *,
         dense_rank() over (partition by customer_id order by order_at)      as order_number,
         case when order_number = 1 then 'New' else 'Repeat' end             as new_vs_repeat
-    from source 
+    from base
+    where order_line_number = 1
 )
 
 select * from result
